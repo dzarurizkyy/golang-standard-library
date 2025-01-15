b5b707bb881696e9a6e46540d9eb7fd285f7bdad
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-01-15 13:28:06 +0700

    section 1 : fmt

---
+++ 001-fmt/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstname := "Dzaru"
+	lastname := "Rizky"
+
+	fmt.Printf("Hello, %s %s 🤗 \n", firstname, lastname)
+	fmt.Println("Hello World 🌎!")
+}
